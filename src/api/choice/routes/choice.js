f22af500c398b61520fb83dcffaaf336712087c5
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2023-05-25 00:00:46 -0300

    Initialized Strapi repo.

---
+++ src/api/choice/routes/choice.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * choice router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::choice.choice');
---
+++ src/api/choice/services/choice.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * choice service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::choice.choice');
